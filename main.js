dffc97024913fda52a1978454c7c1485707d8474
Rohan Thapliyal <thapliyr@oregonstate.edu>
2024-03-26 18:52:38 -0700

    Adding experience page

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 $("#header-home").on('click', function () {
     $("#about-me").hide();
     $("#education").hide();
+    $("#experience").hide();
 });
 $("#header-about").on('click', function () {
     $("#about-me").show();
     $("#education").hide();
+    $("#experience").hide();
 });
 $("#header-education").on('click', function () {
     $("#education").show();
+    $("#about-me").hide();
+    $("#experience").hide();
+});
+$("#header-experience").on('click', function () {
+    $("#experience").show();
+    $("#education").hide();
     $("#about-me").hide();
 });
